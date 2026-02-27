1e30113710df9e9d093bd29dc899ccc02134cdcf
beardsleycaleb3-afk <beardsleycaleb3@gmail.com>
2026-02-27 07:01:56 -0500

    Update and rename sw.js to service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+// service-worker.js
+const CACHE_NAME = 'bhsultan-root-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './sultan.css',
+  './core.js',
+  './hud.js',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(c => c.addAll(FILES_TO_CACHE))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(resp => resp || fetch(event.request))
+  );
+});
