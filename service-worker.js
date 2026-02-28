8437e062ec402ef1ffe5ef92cc1578e07da21989
beardsleycaleb3-afk <beardsleycaleb3@gmail.com>
2026-02-28 12:34:06 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,30 @@
-// service-worker.js
-const CACHE_NAME = 'bhsultan-root-v1';
-const FILES_TO_CACHE = [
-  './',
-  './index.html',
-  './sultan.css',
-  './core.js',
-  './hud.js',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+const CACHE_NAME = 'sultan-v1-checksum';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './beethoven_5.mp3',
+    'https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js'
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(c => c.addAll(FILES_TO_CACHE))
-      .then(() => self.skipWaiting())
-  );
+// Header 1: Install & Cache
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
-    )
-  );
-  self.clients.claim();
+// Footer 1: Cleanup old caches
+self.addEventListener('activate', (e) => {
+    e.waitUntil(caches.keys().then(keys => Promise.all(
+        keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null)
+    )));
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(resp => resp || fetch(event.request))
-  );
+// Header 2: Fetch Intercept
+self.addEventListener('fetch', (e) => {
+    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
+
+// Footer 2: Final Loop Checksum
+self.addEventListener('message', (e) => {
+    if (e.data === 'ping') e.source.postMessage('pong-checksum-v1');
 });
